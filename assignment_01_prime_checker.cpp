2c740177794b16ec65f5156d4c939c6fa835f755
HP <zigahkelvin@gmail.com>
2026-08-02 15:46:11 +0000

    Prime checker app

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -37,3 +37,31 @@
 #include <iostream>
 using namespace std;
 
+bool isPrime(int n) {
+    if (n < 2) {
+        return false;
+    }
+
+    for (int divisor = 2; divisor * divisor <= n; divisor++) {
+        if (n % divisor == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if (isPrime(number)) {
+        cout << number << " is a prime number." << endl;
+    } else {
+        cout << number << " is NOT a prime number." << endl;
+    }
+
+    return 0;
+}
